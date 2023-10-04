3d017b784d6dc5920ff98934c005647e5853b50c
Saulo J <sauloseculoxx@gmail.com>
2023-10-04 19:00:36 -0300

    BugFix RandomImg function, workaround echo script tag on index php after footer

--- TecnoSmart/Clients/AplicadoraSemSono/index.php
+++ TecnoSmart/Clients/AplicadoraSemSono/index.php
@@ -5,4 +5,9 @@
 require('header.php'); 
 require('contentIndex.html');
 require('footer.php'); 
+echo("
+    <script>
+        window.onload(setInterval(changerRandomImg,3500)) 
+    </script>
+    ")
 ?>
