671ee2ba5972f67626f46e2f5ea02332973ff927
Saulo J <sauloseculoxx@gmail.com>
2023-11-08 12:46:42 -0300

    Tag GoogleAnalytics employment

--- TecnoSmart/Clients/AplicadoraSemSono/header.php
+++ TecnoSmart/Clients/AplicadoraSemSono/header.php
@@ -7,6 +7,15 @@
 <!DOCTYPE html>
 <html lang="pt">
     <head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TZHTL265GP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TZHTL265GP');
+</script>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
