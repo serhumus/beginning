87bc1447aa3ae8a8f49c806fff8d743d6b602f15
Saulo J <sauloseculoxx@gmail.com>
2024-01-19 12:22:01 -0300

    Employment of work around the issues to metric conversions

--- TecnoSmart/Clients/AplicadoraSemSono/whatsapp.php
+++ TecnoSmart/Clients/AplicadoraSemSono/whatsapp.php
@@ -1,3 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h1 class="pt-5">Whatsapp</h1>
+    <div class="d-grid">
+        <div class="p-1 ">
+            <p>Toque ou clique <a href="http://wa.me/5511999670572">aqui</a> para ser direcionado ao chat, ou adicione nosso número na tua agenda para iniciar uma conversa: 11 999 670 572 </p> 
+        </div>
+    </div>
+    <p>Ficaremos felizes em ser úteis</p>
+</div>
 <div class="container pt-5">
 <div class="b-example-divider"></div>          
   <div class="row bg-dark cardtecnosmart" style="max-height: 210px; min-height: 210px;">
@@ -28,7 +38,6 @@
   </div>
   <div class="b-example-divider"></div> 
 </div>
-<a href="whatsapp.php"><img alt="Link direto para o WhatsApp" class="WhatsAppButton" style="display: none;" src="../assets/footer/WhatsappLogo100x102.webp" data-recalc-dims="1"></a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 <script src="mainFunctions.js"></script>
